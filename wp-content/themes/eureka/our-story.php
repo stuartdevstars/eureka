4afea4e7d1f5a60c11a1715a10bfc78ab818a629
Chris Pattle <chris.pattle@gmail.com>
2016-06-13 12:27:12 +0100

    Our story page

--- wp-content/themes/eureka/our-story.php
+++ wp-content/themes/eureka/our-story.php
@@ -6,4 +6,32 @@ Template Name: Our story
 
 <?php get_header(); ?>
 
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div id="our-story">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/heading.png" alt="" class="heading">
+				<div class="intro-text">
+					<span>Hello there!</span> Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag na aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea comm odo consequat, duis aute irure dolor
+				</div>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map.png" alt="" class="map">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/photo.png" alt="" class="photo">
+				<div class="fish-text">
+					<span>In search of the perfect fish</span> Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag na aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea comm odo
+				</div>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/cows.png" alt="" class="cows">
+				<div class="beef-text">
+					<span>In search of the perfect beef</span> Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag na aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. laboris nisi ut aliquip ex ea comm odo
+				</div>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map-small.png" alt="" class="map-small">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/fareshare.jpg" alt="" class="fareshare">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/trail.png" alt="" class="trail">
+				<div class="final-text">
+					<span>?</span>Who knows where the adventure take us!
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php get_footer(); ?>
