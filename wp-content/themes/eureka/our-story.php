0a13db74cec721668db21d0d855119aa3376e7c6
Chris Pattle <chris.pattle@gmail.com>
2016-06-15 08:49:49 +0100

    Making our story page responsive

--- wp-content/themes/eureka/our-story.php
+++ wp-content/themes/eureka/our-story.php
@@ -10,25 +10,25 @@ Template Name: Our story
 	<div class="row">
 		<div class="col-sm-12">
 			<div id="our-story">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/heading.png" alt="" class="heading">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/heading.png" alt="" class="heading img-responsive">
 				<div class="intro-text">
 					<span>Meet Richie!</span> , our chief explorer. Not your normal globe-trotter, Richie has dived (sometimes literally) into the markets, harbours and kitchens of the world to bring home its tastiest treasures.
 							</div>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map.png" alt="" class="map">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/explorer.png" alt="" class="explorer">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/photo.png" alt="" class="photo">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map.png" alt="" class="map img-responsive">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/explorer.png" alt="" class="explorer img-responsive hidden-xs">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/photo.png" alt="" class="photo img-responsive">
 				<div class="fish-text">
 					<span>The Quest for the finest fish:</span> Richie knew it’s the cold currents that contain the tastiest Hake. So he put on his bobble hat and headed for Alaska. Eureka! He’d also heard tell of the Arctic current that sweeps up the side of Namibia, so he made friends with the fishermen there too. You can taste his success today!	
 					</div>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/cows.png" alt="" class="cows">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/cows.png" alt="" class="cows img-responsive">
 				<div class="beef-text">
 					<span>The story of Beef at its Best</span>: it’s simple really. Europe’s is the tastiest, best bred, best cut beef around. So why look anywhere further? Finding the right suppliers and charming them with his worldly knowledge of good food was all Richie needed to secure yet another tasty treasure for the Eureka Cove range. 
 				</div>
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map-small.png" alt="" class="map-small">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/fareshare.png" alt="" class="fareshare">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/trail.png" alt="" class="trail">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/map-small.png" alt="" class="map-small img-responsive">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/fareshare.png" alt="" class="fareshare img-responsive">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/our-story/trail.png" alt="" class="trail hidden-xs">
 				<div class="final-text">
-					<span>?</span>Richie loves travelling, and loves home too. That’s why he’s been donating food to the UK food-donating charity Fare Share. 2 whole tonnes of it at last count!
+					<span class="hidden-xs">?</span>Richie loves travelling, and loves home too. That’s why he’s been donating food to the UK food-donating charity Fare Share. 2 whole tonnes of it at last count!
 Richie’s always travelling - so wait and see what’s next!
 				</div>
 			</div>
